25e1f606db2ea308e13f80b94e9d0193ebe09cf6
Idan Atar <idanatar@MacBook-Pro.018.co.il>
2022-03-14 12:11:38 +0200

    Added heroku files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python nodes_monitor.py
+worker: python nodes_monitor.py 
---
+++ heroku-deploy.sh
@@ -0,0 +1,4 @@
+heroku git:remote -a status-to-elastic
+git add .
+git commit -m "Deployment commit"
+git push heroku master 
